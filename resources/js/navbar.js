51864033e167ebe7a2dcc127d764c599bc4aee66
Jack Covino <jackcovino10@gmail.com>
2025-09-18 15:19:13 -0400

    Add files via upload

--- resources/js/navbar.js
+++ resources/js/navbar.js
@@ -1,32 +1,32 @@
 function loadNavbar() {
-    const container = document.getElementById('navbar-container');
-    // Create nav element
-    const nav = document.createElement('nav');
-    nav.className = 'navbar navbar-expand-lg navbar-light bg-light';
-    // Create div
-    const navDiv = document.createElement('div');
-    navDiv.className = 'collapse navbar-collapse';
-    navDiv.id = 'navbarNav';
-    // Create ul
-    const ul = document.createElement('ul');
-    ul.className = 'navbar-nav';
-    // Create nav items
-    const navItems = [
-        { text: 'Home', href: 'index.html' },
-        { text: 'Hobbies', href: 'hobbies.html' }
-    ];
-    navItems.forEach(item => {
-        const li = document.createElement('li');
-        li.className = 'nav-item';
-        const a = document.createElement('a');
-        a.className = 'nav-link';
-        a.href = item.href;
-        a.textContent = item.text;
-        li.appendChild(a);
-        ul.appendChild(li);
-    });
-    // Assemble the navbar
-    navDiv.appendChild(ul);
-    nav.appendChild(navDiv);
-    container.appendChild(nav)
+  const container = document.getElementById('navbar-container');
+  const nav = document.createElement('nav');
+  nav.className = 'navbar navbar-expand-lg navbar-light bg-light';
+
+  const navDiv = document.createElement('div');
+  navDiv.className = 'collapse navbar-collapse';
+  navDiv.id = 'navbarNav';
+
+  const ul = document.createElement('ul');
+  ul.className = 'navbar-nav';
+
+  const navItems = [
+    { text: 'Home', href: '/index.html' },
+    { text: 'Hobbies', href: '/hobbies.html' }
+  ];
+
+  navItems.forEach(item => {
+    const li = document.createElement('li');
+    li.className = 'nav-item';
+    const a = document.createElement('a');
+    a.className = 'nav-link';
+    a.href = item.href;   // root-relative
+    a.textContent = item.text;
+    li.appendChild(a);
+    ul.appendChild(li);
+  });
+
+  navDiv.appendChild(ul);
+  nav.appendChild(navDiv);
+  container.appendChild(nav);
 }
